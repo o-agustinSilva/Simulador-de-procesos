06ceac33ecded047e7727124951a0de9bd98bce0
o-agustinSilva <agustinsilva1009@gmail.com>
2023-10-15 17:47:42 -0300

    Estado finalizado, listo para entregar

--- procesos.js
+++ procesos.js
@@ -10,7 +10,12 @@ var colaTerminados = [];
 var colaAux = [];
 var contador = 0; //Tcp que se resetea y decrece
 var q = 0;
+var cpuDesocupada = 0;
+var cpuOcupadaPorSO = 0; //Se puede calcular fuera de la simulación
+var output = "";
 
+var enlaceDescarga; // Declarar la variable fuera de la función
+var mensajesConcatenados = ''; // Variable para mantener un registro de los mensajes
 /***********************
 **Definición de clases**
 ***********************/
@@ -28,8 +33,10 @@ class Proceso {
         this.tiempoEjecucionAux = duracionR;
         this.arrivalTimeReal    = arrivalTime;
         this.iteracionES        = inputR;
-        this.tiempoInicio       = null;
         this.tiempoFinalizacion = null;
+        this.tiempoRetorno      = null;
+        this.tiempoEnListo      = 0;
+        this.cantidadRAux       = cantidadR;
     }
 }
 
@@ -71,8 +78,36 @@ document.getElementById("fileInput").addEventListener("change", function(event) 
     }
 
     fr.readAsText(this.files[0]);
+});
 
-})
+const agregarActionListener = () => {
+    document.getElementById("boton-descargar").addEventListener("click", function(event) {
+        // Create an <a> element
+        const link = document.createElement("a");
+
+        // Create a Blob object with the file content
+        const file = new Blob([output], { type: 'text/plain' });
+
+        // Set the Blob as the href
+        link.href = URL.createObjectURL(file);
+
+        // Set the download attribute to specify the filename
+        link.download = "resultados.txt";
+
+        // Trigger a click event on the <a> element to initiate the download
+        link.click();
+        URL.revokeObjectURL(link.href);
+    });
+}
+
+const generarOutput = (mensaje) => {
+    if (output === "") { 
+        output = mensaje;
+    }
+    else { 
+        output += "\n" + mensaje;
+    }
+}
 
 /* Ejecuta en función de la planificación */
 function simularProceso() {
@@ -100,7 +135,11 @@ function simularProceso() {
             simularSRT(tip, tfp, tfp);
             break;
     }
+    alert("Se ejecutó la tanda de procesos correctamente.");
+    agregarActionListener();
+
 }
+
 
 // Elimina todos los elementos del array usando splice
 const clearArray = (a) => {
@@ -145,7 +184,7 @@ const runningHandler = (tiempo) => {
     if (colaCorriendo.length == 0 ) 
         if (colaListos.length != 0 && contador == 0) {
             colaCorriendo.push(colaListos.shift());
-            console.log("El proceso " + colaCorriendo[0].nombre + " entró a ejecutarse en el tiempo " + tiempo);
+            generarOutput("El proceso " + colaCorriendo[0].nombre + " entró a ejecutarse en el tiempo " + tiempo);
         } else if (contador > 0){ 
             contador--;
         }
@@ -157,7 +196,7 @@ const runningHandler = (tiempo) => {
 const checkRunningProcess = (tiempo, tcp) => {
     if (colaCorriendo[0].tiempoEjecucionAux === 0){ 
         colaCorriendo[0].cantidadR--;
-        console.log("El proceso " + colaCorriendo[0].nombre + " se bloquéo en el tiempo " + tiempo);
+        generarOutput("El proceso " + colaCorriendo[0].nombre + " se bloquéo en el tiempo " + tiempo); 
         colaCorriendo[0].iteracionES--;
         colaBloqueados.push(colaCorriendo.shift());
         contador = tcp;
@@ -171,12 +210,12 @@ const checkBlockedProcess = (tiempo, tfp) => {
                 colaTerminados.push(p);
                 colaAux.push(p);
                 p.tiempoFinalizacion = parseInt(tiempo) + parseInt(tfp);
-                console.log("El proceso " + p.nombre + " finalizó en el tiempo " + p.tiempoFinalizacion);
+                generarOutput("El proceso " + p.nombre + " finalizó en el tiempo " + p.tiempoFinalizacion);
             }
             else { //Desbloqueo el proceso
                 colaListos.push(p); 
                 colaAux.push(p);
-                console.log("El proceso " + p.nombre + " se desbloqueó en el tiempo " + tiempo);
+                generarOutput("El proceso " + p.nombre + " se desbloqueó en el tiempo " + tiempo);
                 p.tiempoEjecucionAux = p.duracionR;
                 p.iteracionES = p.inputR;
             }
@@ -188,6 +227,11 @@ const checkBlockedProcess = (tiempo, tfp) => {
         deleteFromArray(colaBloqueados, colaAux);
         clearArray(colaAux);
     }
+}
+
+const sumarEstadoListo = () => {
+    for (const p of colaListos) 
+        p.tiempoEnListo++;
 }
 
 //---------------------------- FCFS  ------------------------------------------------
@@ -214,10 +258,21 @@ const simularFCFS = (tip, tfp, tcp) => {
 
         runningHandler(tiempo);
 
+        if (colaListos.length > 0) 
+            sumarEstadoListo();
+
         if (colaBloqueados.length > 0) 
             checkBlockedProcess(tiempo, tfp);
         tiempo++;
+
+        if (colaCorriendo.length == 0) {
+            cpuDesocupada++;
+        }
     }
+
+    cargarTablaProcesos();
+    cargarTablaTanda();
+    cargarTablaCPU(tiempo-1);
 }
 
 //---------------------- PRIORIDAD EXTERNA ------------------------------------------
@@ -226,7 +281,7 @@ const highestPriorityProcess = (arrayOfProcess) => {
     let procesoPrioridadMaxima = arrayOfProcess[0];
 
     for (const p of arrayOfProcess) {
-        if (p.prioridad < procesoPrioridadMaxima.prioridad) {
+        if (p.prioridad > procesoPrioridadMaxima.prioridad) {
             procesoPrioridadMaxima = p;
         }
     }
@@ -240,7 +295,7 @@ const priorityRunningHandler = (tiempo) => {
         if (colaListos.length != 0 && contador == 0) {
             colaCorriendo.push(highestPriorityProcess(colaListos));
             colaListos.splice(colaListos.indexOf(highestPriorityProcess(colaListos)), 1);
-            console.log("El proceso " + colaCorriendo[0].nombre + " entró a ejecutarse en el tiempo " + tiempo);
+            generarOutput("El proceso " + colaCorriendo[0].nombre + " entró a ejecutarse en el tiempo " + tiempo);
         } else if (contador > 0){ 
             contador--;
             alert("se restó al contador, es "+ contador + " en tiempo " + tiempo);
@@ -248,6 +303,54 @@ const priorityRunningHandler = (tiempo) => {
 
     //Resto uno a la ejecución
     if (colaCorriendo.length == 1) colaCorriendo[0].tiempoEjecucionAux--;
+}
+
+const permutarProcesosPriority = () => {
+    //Se entra chequeando que la cola de listos no está vacía
+
+    //Obntengo el proceso con menor tiempo de ejecución restante en cola de listos
+    let p = highestPriorityProcess(colaListos);
+
+    //Si hay un proceso corriendo y ese proceso tiene mayor prioridad, los permuto
+    if (colaCorriendo.length == 1)
+        if (p.prioridad > colaCorriendo[0].prioridad) {
+            //Permuto los procesos
+            colaListos.push(colaCorriendo.shift());
+            colaCorriendo.push(p);
+
+            //Elimino el proceso de la cola de listos
+            colaListos.splice(colaListos.indexOf(p), 1);
+        }
+}
+
+const checkBlockedProcessPriority = (tiempo, tfp) => {
+    for (const p of colaBloqueados) {
+        if (p.iteracionES === 0) {
+            if (p.cantidadR === 0) {
+                colaTerminados.push(p);
+                colaAux.push(p);
+                p.tiempoFinalizacion = parseInt(tiempo) + parseInt(tfp);
+                generarOutput("El proceso " + p.nombre + " finalizó en el tiempo " + p.tiempoFinalizacion);
+            }
+            else { //Desbloqueo el proceso
+                colaListos.push(p); 
+                colaAux.push(p);
+
+                generarOutput("El proceso " + p.nombre + " se desbloqueó en el tiempo " + tiempo);
+
+                p.tiempoEjecucionAux = p.duracionR;
+                p.iteracionES = p.inputR;
+
+                permutarProcesosPriority();
+            }
+        } else 
+            p.iteracionES--;
+    }
+
+    if (colaAux.length > 0) {
+        deleteFromArray(colaBloqueados, colaAux);
+        clearArray(colaAux);
+    }
 }
 
 const simularPrioridad = (tip, tfp, tcp) => {
@@ -272,11 +375,18 @@ const simularPrioridad = (tip, tfp, tcp) => {
 
         priorityRunningHandler(tiempo);
 
+        if (colaListos.length > 0) 
+            sumarEstadoListo();
+
         if (colaBloqueados.length > 0) 
-            checkBlockedProcess(tiempo, tfp);
+            checkBlockedProcessPriority(tiempo, tfp);
         
         tiempo++;
     }
+
+    cargarTablaProcesos();
+    cargarTablaTanda();
+    cargarTablaCPU(tiempo-1);
 }
 
 //---------------------- ROUND ROBIN ------------------------------------------
@@ -303,7 +413,7 @@ const checkRunningProcessRR = (tiempo, tcp, quantum) => {
             //Ajusto sus valores
             colaCorriendo[0].cantidadR--;
             colaCorriendo[0].iteracionES--;
-            console.log("El proceso " + colaCorriendo[0].nombre + " se bloquéo en el tiempo " + tiempo);
+            generarOutput("El proceso " + colaCorriendo[0].nombre + " se bloquéo en el tiempo " + tiempo);
 
             //Bloqueo el proceso
             colaBloqueados.push(colaCorriendo.shift());
@@ -342,11 +452,17 @@ const simularRoundRobin = (tip, tfp, tcp, quantum) => {
 
         roundRobinRunningHandler(tiempo, q);
 
+        if (colaListos.length > 0) 
+            sumarEstadoListo();
+
         if (colaBloqueados.length > 0) 
             checkBlockedProcess(tiempo, tfp);
         
         tiempo++;
     }
+
+    cargarTablaProcesos();
+    cargarTablaTanda();
 }
 
 //---------------------- SHORTEST PROCESS NEXT ------------------------------------------
@@ -369,7 +485,7 @@ const spnRunningHandler = (tiempo) => {
         if (colaListos.length != 0 && contador == 0) {
             colaCorriendo.push(shortestProcess(colaListos));
             colaListos.splice(colaListos.indexOf(shortestProcess(colaListos)), 1);
-            console.log("El proceso " + colaCorriendo[0].nombre + " entró a ejecutarse en el tiempo " + tiempo);
+            generarOutput("El proceso " + colaCorriendo[0].nombre + " entró a ejecutarse en el tiempo " + tiempo);
         } else if (contador > 0){ 
             contador--;
             alert("se restó al contador, es "+ contador + " en tiempo " + tiempo);
@@ -401,11 +517,18 @@ const simularSPN = (tip, tfp, tcp) => {
 
         spnRunningHandler(tiempo);
 
+        if (colaListos.length > 0) 
+            sumarEstadoListo();
+
         if (colaBloqueados.length > 0) 
             checkBlockedProcess(tiempo, tfp);
         
         tiempo++;
     }
+
+    cargarTablaProcesos();
+    cargarTablaTanda();
+    cargarTablaCPU(tiempo-1);
 }
 
 //---------------------- SHORTEST REMAINING TIME ------------------------------------------
@@ -422,10 +545,10 @@ const srtProcess = (arrayOfProcess) => {
     return srtp;
 }
 
-const permutarProcesos = (tiempo) => {
+const permutarProcesos = () => {
     //Se entra chequeando que la cola de listos no está vacía
 
-    //Obtnego el proceso con menor tiempo de ejecución restante en cola de listos
+    //Obntengo el proceso con menor tiempo de ejecución restante en cola de listos
     let p = srtProcess(colaListos);
 
     //Si hay un proceso corriendo y ese proceso tiene mayor tiempo de ejecución restante, los permuto
@@ -447,7 +570,7 @@ const srtRunningHandler = (tiempo) => {
         if (colaListos.length != 0 && contador == 0) {
             colaCorriendo.push(srtProcess(colaListos));
             colaListos.splice(colaListos.indexOf(srtProcess(colaListos)), 1);
-            console.log("El proceso " + colaCorriendo[0].nombre + " entró a ejecutarse en el tiempo " + tiempo);
+            generarOutput("El proceso " + colaCorriendo[0].nombre + " entró a ejecutarse en el tiempo " + tiempo);
         } else if (contador > 0){ 
             contador--;
             alert("se restó al contador, es "+ contador + " en tiempo " + tiempo);
@@ -464,18 +587,18 @@ const checkBlockedProcessSRT = (tiempo, tfp) => {
                 colaTerminados.push(p);
                 colaAux.push(p);
                 p.tiempoFinalizacion = parseInt(tiempo) + parseInt(tfp);
-                console.log("El proceso " + p.nombre + " finalizó en el tiempo " + p.tiempoFinalizacion);
+                generarOutput("El proceso " + p.nombre + " finalizó en el tiempo " + p.tiempoFinalizacion);
             }
             else { //Desbloqueo el proceso
                 colaListos.push(p); 
                 colaAux.push(p);
 
-                console.log("El proceso " + p.nombre + " se desbloqueó en el tiempo " + tiempo);
+                generarOutput("El proceso " + p.nombre + " se desbloqueó en el tiempo " + tiempo);
 
                 p.tiempoEjecucionAux = p.duracionR;
                 p.iteracionES = p.inputR;
 
-                permutarProcesos(tiempo);
+                permutarProcesos();
             }
         } else 
             p.iteracionES--;
@@ -509,9 +632,120 @@ const simularSRT = (tip, tfp, tcp) => {
 
         srtRunningHandler(tiempo);
 
+        if (colaListos.length > 0) 
+            sumarEstadoListo();
+
         if (colaBloqueados.length > 0) 
             checkBlockedProcessSRT(tiempo, tfp);
         
         tiempo++;
     }
+
+    cargarTablaProcesos();
+    cargarTablaTanda();
+    cargarTablaCPU(tiempo-1);
+}
+
+//---------------------- CALCULO DE VALORES ------------------------------------------
+
+// Carga de la tabla de indicadores de procesos
+const cargarTablaProcesos = () => {
+    let tabla = document.getElementById('indicadores-proceso');
+
+    cantProcesos = colaTerminados.length;
+    colaTerminados.sort((a, b) => (a.nombre < b.nombre) ? -1 : (a.nombre > b.nombre) ? 1 : 0);
+
+    for (const p of colaTerminados) {
+
+        //Calculo tiempos
+        const tiempoRetorno = p.tiempoFinalizacion - p.arrivalTime;
+        const aux = tiempoRetorno / (p.cantidadRAux * p.duracionR);
+        const tiempoRetornoNormalizado = aux.toFixed(2);
+
+        //Creo la fila
+        const fila = tabla.insertRow();
+
+        // Crea celdas y asigna valores a las celdas
+        const idCelda = fila.insertCell();
+        idCelda.textContent = p.nombre;
+
+        const tiempoRetornoCelda = fila.insertCell();
+        tiempoRetornoCelda.textContent = tiempoRetorno;
+
+        const tiempoRetornoNormalizadoCelda = fila.insertCell();
+        tiempoRetornoNormalizadoCelda.textContent = tiempoRetornoNormalizado;
+
+        const tiempoEnEstadoListoCelda = fila.insertCell();
+        tiempoEnEstadoListoCelda.textContent = p.tiempoEnListo;
+    }
+}
+
+// Carga de la tabla de indicadores de la tanda
+const cargarTablaTanda = () => {
+    let tabla = document.getElementById('indicadores-tanda');
+
+    cantProcesos = colaTerminados.length;
+    colaTerminados.sort((a, b) => (a.nombre < b.nombre) ? -1 : (a.nombre > b.nombre) ? 1 : 0);
+
+        //Calculo el tiempo de retorno de la tanda
+        const mayorTiempoFin = colaTerminados.reduce((prev, current) => (prev.tiempoFinalizacion > current.tiempoFinalizacion) ? prev : current);
+        const menorTiempoArribo = colaTerminados.reduce((prev, current) => (prev.arrivalTime < current.arrivalTime) ? prev : current);
+        
+        const tiempoRetornoTanda = mayorTiempoFin.tiempoFinalizacion - menorTiempoArribo.arrivalTime;
+        
+        //Calculo el tiempo de retorno de la tanda normalizado
+        let sum = 0;
+        for (const p of colaTerminados) 
+            sum += (p.tiempoFinalizacion - p.arrivalTime);
+        
+        const aux = sum / colaTerminados.length;
+        const tiempoRetornoTandaN = aux.toFixed(2);
+
+        //Creo la fila
+        const fila = tabla.insertRow();
+
+        // Crea celdas y asigna valores a las celdas
+        const tRetorno = fila.insertCell();
+        tRetorno.textContent = tiempoRetornoTanda;
+        const tRetornoNormalizado = fila.insertCell();
+        tRetornoNormalizado.textContent = tiempoRetornoTandaN;   
+}
+
+// Carga de la tabla de indicadores de la tanda
+const cargarTablaCPU = (tiempo) => {
+    let tabla = document.getElementById('indicadores-cpu');
+
+    cantProcesos = colaTerminados.length;
+    colaTerminados.sort((a, b) => (a.nombre < b.nombre) ? -1 : (a.nombre > b.nombre) ? 1 : 0);
+
+        //Calculo el tiempo de CPU utilizado por el SO
+        let cpuSO = 0;
+        for (const p of colaTerminados) 
+            cpuSO+= (p.cantidadRAux * p.inputR);
+
+        //Calculo el tiempo de CPU utilizado por los procesos
+        let cpuP = 0;
+        for (const p of colaTerminados) 
+            cpuP+= (p.cantidadRAux * p.duracionR);
+        
+
+        //Tiempo de CPU utilizado por procesos en porcentaje
+        const aux = (cpuP / tiempo);
+        const cpuPorcentaje = aux.toFixed(2);
+
+        //Creo la fila
+        const fila = tabla.insertRow();
+
+        // Crea celdas y asigna valores a las celdas
+        const cpuD = fila.insertCell();
+        cpuD.textContent = cpuDesocupada;
+
+        const cpuS = fila.insertCell();
+        cpuS.textContent = cpuSO;   
+
+        const cpuPA = fila.insertCell();
+        cpuPA.textContent = cpuP;   
+
+        const cpuPP = fila.insertCell();
+        cpuPP.textContent = cpuPorcentaje; 
 }
